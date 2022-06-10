06f91a08103baa3a79ee3ce813f8e4016b7f5e2f
gwq5210 <gwq5210@qq.com>
2022-06-10 16:20:21 +0800

    支持map,set

--- src/json_parser.h
+++ src/json_parser.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <rapidjson/document.h>
-
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/rapidjson.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
 #include <spdlog/spdlog.h>
+
 #include <cassert>
 #include <functional>
+#include <map>
+#include <set>
 #include <type_traits>
 #include <unordered_map>
-
+#include <unordered_set>
 
 #include "parser.h"
 
@@ -21,11 +27,40 @@ class JsonParser : public Parser {
   }
 
   template <typename T>
-  static typename std::enable_if<FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_config,
+  static bool ParseFromJsonString(const std::string& json_str, T& value) {
+    rapidjson::Document json_config;
+    json_config.Parse(json_str);
+    if (json_config.HasParseError()) {
+      return false;
+    }
+    return value.ParseFromJsonValue(json_config);
+  }
+
+  template <typename T>
+  static bool SerializeToJsonString(const T& value, std::string& json_str, bool pretty = false) {
+    rapidjson::Value::AllocatorType allocator;
+    rapidjson::Value json_config(rapidjson::kObjectType);
+    if (!value.SerializeToJsonValue(json_config, allocator)) {
+      return false;
+    }
+    rapidjson::StringBuffer buffer;
+    if (pretty) {
+      rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+      json_config.Accept(writer);
+    } else {
+      rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+      json_config.Accept(writer);
+    }
+    json_str.assign(buffer.GetString());
+    return true;
+  }
+
+  template <typename T>
+  static typename std::enable_if<FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
                                                                         const std::string& field_name,
                                                                         T& config_value) {
-    auto it = json_config.FindMember(field_name);
-    if (it == json_config.MemberEnd()) {
+    auto it = json_value.FindMember(field_name);
+    if (it == json_value.MemberEnd()) {
       return false;
     }
     const rapidjson::Value& json_field = it->value;
@@ -37,11 +72,11 @@ class JsonParser : public Parser {
   }
 
   template <typename T>
-  static typename std::enable_if<!FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_config,
+  static typename std::enable_if<!FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
                                                                          const std::string& field_name,
                                                                          T& config_value) {
-    auto it = json_config.FindMember(field_name);
-    if (it == json_config.MemberEnd()) {
+    auto it = json_value.FindMember(field_name);
+    if (it == json_value.MemberEnd()) {
       return false;
     }
     const rapidjson::Value& json_field = it->value;
@@ -52,11 +87,11 @@ class JsonParser : public Parser {
   }
 
   template <typename T>
-  static typename std::enable_if<FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_config,
+  static typename std::enable_if<FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
                                                                         const std::string& field_name,
                                                                         std::vector<T>& config_value) {
-    auto it = json_config.FindMember(field_name);
-    if (it == json_config.MemberEnd()) {
+    auto it = json_value.FindMember(field_name);
+    if (it == json_value.MemberEnd()) {
       return false;
     }
     const rapidjson::Value& json_field = it->value;
@@ -73,11 +108,11 @@ class JsonParser : public Parser {
   }
 
   template <typename T>
-  static typename std::enable_if<!FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_config,
+  static typename std::enable_if<!FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
                                                                          const std::string& field_name,
                                                                          std::vector<T>& config_value) {
-    auto it = json_config.FindMember(field_name);
-    if (it == json_config.MemberEnd()) {
+    auto it = json_value.FindMember(field_name);
+    if (it == json_value.MemberEnd()) {
       return false;
     }
     const rapidjson::Value& json_field = it->value;
@@ -95,29 +130,77 @@ class JsonParser : public Parser {
     return true;
   }
 
+  template <typename T, typename Set>
+  static typename std::enable_if<FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
+                                                                        const std::string& field_name,
+                                                                        Set<T>& config_value) {
+    std::vector<T> config_arr;
+    config_value.clear();
+    if (!Parse(json_value, field_name, config_arr)) {
+      return false;
+    }
+    config_value.insert(config_arr.begin(), config_arr.end());
+    return true;
+  }
+
+  template <typename V, typename Map>
+  static typename std::enable_if<FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
+                                                                        const std::string& field_name,
+                                                                        Map<std::string, V>& config_value) {
+    config_value.clear();  
+    if (!json_value.IsObject()) {
+      return false;
+    }
+    for (auto it = json_value.MemberBegin(); it != json_value.MemberEnd(); ++it) {
+      if (it->name.IsString() && it->value.Is<V>()) {
+        config_value.emplace(it->name.GetString(), it->value.Get<V>());
+      }
+    }
+    return true;
+  }
+
+  template <typename V, typename Map>
+  static typename std::enable_if<!FieldType<T>::value, bool>::type Parse(const rapidjson::Value& json_value,
+                                                                        const std::string& field_name,
+                                                                        Map<std::string, V>& config_value) {
+    config_value.clear();  
+    if (!json_value.IsObject()) {
+      return false;
+    }
+    for (auto it = json_value.MemberBegin(); it != json_value.MemberEnd(); ++it) {
+      if (it->name.IsString() && it->value.IsObjcet()) {
+        V item;
+        if (Parse(json_value, it->name.GetString(), item)) {
+          config_value.emplace(it->name.GetString(), std::move(itme));
+        }
+      }
+    }
+    return true;
+  }
+
   template <typename T>
   static typename std::enable_if<FieldType<T>::value, bool>::type Serialize(
-      const std::string& field_name, const T& config_value, rapidjson::Value& json_config,
+      const std::string& field_name, const T& config_value, rapidjson::Value& json_value,
       rapidjson::Value::AllocatorType& allocator) {
     rapidjson::Value json_field;
     json_field.Set(config_value, allocator);
-    json_config.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
+    json_value.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
     return true;
   }
 
   template <typename T>
   static typename std::enable_if<!FieldType<T>::value, bool>::type Serialize(
-      const std::string& field_name, const T& config_value, rapidjson::Value& json_config,
+      const std::string& field_name, const T& config_value, rapidjson::Value& json_value,
       rapidjson::Value::AllocatorType& allocator) {
     rapidjson::Value json_field(rapidjson::kObjectType);
     config_value.SerializeToJsonValue(json_field, allocator);
-    json_config.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
+    json_value.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
     return true;
   }
 
   template <typename T>
   static typename std::enable_if<FieldType<T>::value, bool>::type Serialize(
-      const std::string& field_name, const std::vector<T>& config_value, rapidjson::Value& json_config,
+      const std::string& field_name, const std::vector<T>& config_value, rapidjson::Value& json_value,
       rapidjson::Value::AllocatorType& allocator) {
     rapidjson::Value json_field(rapidjson::kArrayType);
     for (auto& v : config_value) {
@@ -125,13 +208,13 @@ class JsonParser : public Parser {
       item.Set(v, allocator);
       json_field.PushBack(item, allocator);
     }
-    json_config.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
+    json_value.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
     return true;
   }
 
   template <typename T>
   static typename std::enable_if<!FieldType<T>::value, bool>::type Serialize(
-      const std::string& field_name, const std::vector<T>& config_value, rapidjson::Value& json_config,
+      const std::string& field_name, const std::vector<T>& config_value, rapidjson::Value& json_value,
       rapidjson::Value::AllocatorType& allocator) {
     rapidjson::Value json_field(rapidjson::kArrayType);
     for (auto& v : config_value) {
@@ -139,7 +222,7 @@ class JsonParser : public Parser {
       v.SerializeToJsonValue(item, allocator);
       json_field.PushBack(item, allocator);
     }
-    json_config.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
+    json_value.AddMember(rapidjson::Value(field_name, allocator), json_field, allocator);
     return true;
   }
 
@@ -149,5 +232,9 @@ class JsonParser : public Parser {
   JsonParser& operator=(const JsonParser& other) = delete;
   virtual ~JsonParser() {}
 };
+
+#define JSON_PARSE_FIELD(FIELD_NAME) pbconfig::JsonParser::Parse(json_value, #FIELD_NAME, FIELD_NAME)
+
+#define JSON_SERIALIZE_FIELD(FIELD_NAME) pbconfig::JsonParser::Serialize(#FIELD_NAME, FIELD_NAME, json_value, allocator)
 
 }  // namespace pbconfig
--- src/json_parser_test.cc
+++ src/json_parser_test.cc
@@ -4,24 +4,18 @@
 #include "json_parser.h"
 #include "proto/person.cfg.h"
 
-#include "rapidjson/document.h"
-#include "rapidjson/prettywriter.h"
-#include "rapidjson/rapidjson.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/writer.h"
+#include <rapidjson/document.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/rapidjson.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
 
 TEST(JsonParserTest, JsonParserTest) {
   Person person;
+  std::string json_content = pbconfig::ReadFile("testdata/person.json");
   {
-    rapidjson::Document json_config;
-    std::string json_content = pbconfig::ReadFile("testdata/person.json");
-    json_config.Parse(json_content);
-    ASSERT_TRUE(!json_config.HasParseError());
-    pbconfig::JsonParser::Parse(json_config, "name", person.name);
-    pbconfig::JsonParser::Parse(json_config, "age", person.age);
-    pbconfig::JsonParser::Parse(json_config, "address", person.address);
-    pbconfig::JsonParser::Parse(json_config, "phone_number", person.phone_number);
-    pbconfig::JsonParser::Parse(json_config, "address_list", person.address_list);
+    bool ret = pbconfig::JsonParser::ParseFromJsonString(json_content, person);
+    ASSERT_TRUE(ret);
     fmt::print("name {}\n", person.name);
     fmt::print("age {}\n", person.age);
     fmt::print("province {}\n", person.address.province);
@@ -33,16 +27,10 @@ TEST(JsonParserTest, JsonParserTest) {
     }
   }
   {
-    rapidjson::Value::AllocatorType allocator;
-    rapidjson::Value json_config(rapidjson::kObjectType);
-    pbconfig::JsonParser::Serialize("name", person.name, json_config, allocator);
-    pbconfig::JsonParser::Serialize("age", person.age, json_config, allocator);
-    pbconfig::JsonParser::Serialize("address", person.address, json_config, allocator);
-    pbconfig::JsonParser::Serialize("phone_number", person.phone_number, json_config, allocator);
-    pbconfig::JsonParser::Serialize("address_list", person.address_list, json_config, allocator);
-    rapidjson::StringBuffer buffer;
-    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
-    json_config.Accept(writer);
-    fmt::print("{}\n", buffer.GetString());
+    std::string json_str;
+    bool ret = pbconfig::JsonParser::SerializeToJsonString(person, json_str, true);
+    ASSERT_TRUE(ret);
+    fmt::print("{}\n", json_str);
+    EXPECT_EQ(json_content, json_str);
   }
 }
